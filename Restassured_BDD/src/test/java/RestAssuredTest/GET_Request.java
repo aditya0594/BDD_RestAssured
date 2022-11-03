a6b7bbb09dd7b94d5d95bcf2871e7bebc133f95d
Aditya Pawar <adityapawar180@gmail.com>
2022-11-03 18:24:08 +0530

    First setup project all file

--- Restassured_BDD/src/test/java/RestAssuredTest/GET_Request.java
+++ Restassured_BDD/src/test/java/RestAssuredTest/GET_Request.java
@@ -1,15 +1,10 @@
 package RestAssuredTest;
 
+import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-
-import static org.hamcrest.Matchers.*;
-
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class GET_Request {
 		
@@ -23,13 +18,7 @@ public class GET_Request {
 			.statusCode(200)
 			.assertThat()
 			.body("data.name", equalTo("Chandira Kaniyar VM"))
-			.header("Content-Type", "application/json; charset=utf-8");
-			
-//			RequestSpecification httpRequest = RestAssured.given();
-//			Response response = httpRequest.get("/Hyderabad");
-//			JsonPath jsonPathEvaluator = response.jsonPath();
-//			String name = jsonPathEvaluator.get("Chandira Kaniyar VM");
-//			System.out.println("City received from Response " + name);
+			.header("Content-Type", "application/json; charset=utf-8")
 	
   }
 }
